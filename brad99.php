2108140df4eb9795b17d75dab5bd21f0005a359f
hsupeichang1 <joyce81116@hotmail.com>
2019-07-01 16:59:26 +0800

    99乘法表 運算式

--- brad99.php
+++ brad99.php
@@ -2,47 +2,24 @@
 
 
 ?>
-
 <table border='1' width = '100%'>
-<tr>
-    <td>
     <?php
-    for($i = 1; $i <= 9; $i++){
-        $r = 2 * $i; 
-        echo "2 x {$i} = 2<br>";
-    }
-?>
-    2 x 1 =2 <br>
-    2 x 2 =4 <br>
-    2 x 3 =6 <br>
-    2 x 4 =8 <br>
-    2 x 5 =10 <br>
-    2 x 6 =12 <br>
-    2 x 7 =14 <br>
-    2 x 8 =16 <br>
-    2 x 9 =18 <br>
-    </td>
-    <td>
-    3 x 1 =3 <br>
-    3 x 2 =6 <br>
-    3 x 3 =9 <br>
-    3 x 4 =12 <br>
-    3 x 5 =15 <br>
-    3 x 6 =18 <br>
-    3 x 7 =21 <br>
-    3 x 8 =24 <br>
-    3 x 9 =27 <br>
-    </td>
-    4 x 1 =3 <br>
-    4 x 2 =6 <br>
-    4 x 3 =9 <br>
-    4 x 4 =12 <br>
-    4 x 5 =15 <br>
-    4 x 6 =18 <br>
-    4 x 7 =21 <br>
-    4 x 8 =24 <br>
-    4 x 9 =27 <br>
-    </td>
+    define("ROWS", 3);
+    define("FROM", 2); //列
+    define("TO", 5);  //欄
 
-</tr>
-<table>
+    for ($k = 0; $k < ROWS; $k++){
+        echo '<tr>';
+        for($j = FROM; $j<=TO; $j++){
+            echo '<td bgcolor="#333">';
+            $newj = $j + (TO-FROM+1) * $k;
+                 for ($i = 1; $i<=9; $i++){
+                     $r = $newj * $i; 
+                     echo "{$newj} x {$i} = {$r}<br>";
+                    }
+            echo '</td>';
+            }
+        echo '</tr>';
+    }     
+?>
+</table>
