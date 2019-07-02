18a0391e94cdf9b6209ae35ceb816e768392da7e
hsupeichang1 <joyce81116@hotmail.com>
2019-07-02 09:21:20 +0800

    早上測試

---
+++ brad19.php
@@ -0,0 +1,16 @@
+<?php
+$a = [];
+//var_dump($a);
+$a [] = 1;
+$a [] = 'brad';
+$a [] = true;
+$a [] = 12.3; 
+var_dump($a);
+echo count($a) . '<br>';
+$a [10] = 12; $a[1] = 'Eric';
+var_dump ($a);
+$a [] = 123;
+var_dump ($a);
+
+
+?>
